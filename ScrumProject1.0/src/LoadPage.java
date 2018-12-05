01b58af7d98c502f35804ceb26a1863ffedbef0c
Lorens <Lorens@10.117.96.38>
2018-12-04 22:15:47 -0800

    .

--- ScrumProject1.0/src/LoadPage.java
+++ ScrumProject1.0/src/LoadPage.java
@@ -8,11 +8,12 @@ public class LoadPage extends JPanel
 		Container UpperSection = new Container();
 		UpperSection.setLayout(new GridLayout(3,1));
 		
-	//	UpperSection.add(new SearchBar("Text File: ", "Browse"));
+	//  UpperSection.add(new SearchBar("Text File: ", "Browse"));
 	//	UpperSection.add(new SearchBar("Text File: ", "Browse"));
 	//	UpperSection.add(new SearchBar("Text File: ", "Browse"));
 		
 		this.setLayout(new BorderLayout());
 		this.add(UpperSection,BorderLayout.NORTH);
+		
 	}
 }
