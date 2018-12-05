4017c96bdcb4406cd3109f1764b150c2e53466dc
CHANDAN <CHANDAN@10.117.133.146>
2018-12-05 15:45:50 -0800

    started the load page.

--- ScrumProject1.0/src/LoadPage.java
+++ ScrumProject1.0/src/LoadPage.java
@@ -9,8 +9,27 @@ public class LoadPage extends JPanel
 		Container UpperSection = new Container();
 		UpperSection.setLayout(new GridLayout(3,1));
 		
-	//  UpperSection.add(new SearchBar("Text File: ", "Browse"));
-	//	UpperSection.add(new SearchBar("Text File: ", "Browse"));
+	    UpperSection.add(new SearchBar("Text File: ", "Browse"));
+	  
+	    
+	    Container pane = new Container();
+	    pane.setLayout(new BorderLayout(10,10));
+	    
+	    pane.add(new Button("Left"), BorderLayout.WEST);
+	    
+	    pane.add(new Button("Center"), BorderLayout.CENTER);
+	    
+	    
+	    UpperSection.add(pane);
+	    
+	    
+	    
+	    
+	    
+	    
+	    //UpperSection.setLayout(border); //border layout for the container
+	    //UpperSection.add(new testPanel, BorderLayout.CENTER);
+	  
 	//	UpperSection.add(new SearchBar("Text File: ", "Browse"));
 		
 		this.setLayout(new BorderLayout());
