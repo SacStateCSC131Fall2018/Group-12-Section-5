49f41aaa2ae2faae27f59e33213475330d3cac8c
Lorens <Lorens@10.117.96.38>
2018-12-04 19:42:24 -0800

    Added Summary Page

--- ScrumProject1.0/src/SummPage.java
+++ ScrumProject1.0/src/SummPage.java
@@ -10,10 +10,11 @@ public class SummPage extends JPanel {
 	}
 	public void setupSummPage()
 	{
+	
 	this.setLayout(new BorderLayout(10,10));
 	JTextArea fileContents = new JTextArea();
 	fileContents.setEditable(false);
-	
+
 	//Make text Scrollable
 	JScrollPane summeryPageSumm = new JScrollPane(fileContents);
 	summeryPageSumm.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
