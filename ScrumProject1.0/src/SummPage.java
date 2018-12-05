e9c2363946c0224731391c4e5fc811b55d034386
Lorens <Lorens@10.117.96.38>
2018-12-04 19:44:21 -0800

    Added Summary Page.

--- ScrumProject1.0/src/SummPage.java
+++ ScrumProject1.0/src/SummPage.java
@@ -10,7 +10,6 @@ public class SummPage extends JPanel {
 	}
 	public void setupSummPage()
 	{
-	
 	this.setLayout(new BorderLayout(10,10));
 	JTextArea fileContents = new JTextArea();
 	fileContents.setEditable(false);
